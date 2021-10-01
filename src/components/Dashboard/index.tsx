4bc203c60ebabba99b52c79dde6edc5ae72b067a
Evandro Brandão <evandro@zoetechnology.com.br>
2021-10-01 17:51:27 -0300

    Initial commit

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from './styles'
+
+import { Summary } from '../Summary'
+import { TrasactionsTable } from '../TransactionsTable'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+      <TrasactionsTable />
+    </Container>
+  )
+}
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,45 @@
+import { Container } from './styles'
+
+export function TrasactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Aluguel</td>
+            <td className='withdraw'>- R$1.000,00</td>
+            <td>Casa</td>
+            <td>06/10/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvomento de website</td>
+            <td className='deposit'>R$12.000,00</td>
+            <td>Freela</td>
+            <td>10/10/2021</td>
+          </tr>
+          <tr>
+            <td>Parcela da viagem</td>
+            <td className='withdraw'>- R$2.000,00</td>
+            <td>Lazer</td>
+            <td>15/10/2021</td>
+          </tr>
+          <tr>
+            <td>Venda do PS4</td>
+            <td className='deposit'>R$1.200,00</td>
+            <td>Extra</td>
+            <td>21/10/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
