70832a47db19d066a13e31ad3917206dddb5a461
devbran <evandro.brandao@lighthouseit.com.br>
2022-01-04 21:59:16 -0300

    feat(transactions modal): input form builded

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,7 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
+
+import closeImg from '../../assets/close.svg'
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -11,9 +14,36 @@ export function NewTransactionModal({ isOpen,
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
 
-      <h2>Cadastrar Transação</h2>
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input
+          placeholder="Título"
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
 
     </Modal>
   )
